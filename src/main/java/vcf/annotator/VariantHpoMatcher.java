7fbd15400c9bc93dfdae4b64e64fef223d302190
JonathanKlimp <78917640+JonathanKlimp@users.noreply.github.com>
2022-04-04 15:15:29 +0200

    added documentation

--- src/main/java/vcf/annotator/VariantHpoMatcher.java
+++ src/main/java/vcf/annotator/VariantHpoMatcher.java
@@ -7,12 +7,18 @@ import java.util.Scanner;
 
 public class VariantHpoMatcher {
 
+    /**
+     * Method that gets a variant string and returns a list with HPO terms
+     * associated with the gene in the variant.
+     * @param variant Sting of a variant from a vcf file
+     * @return ArrayList with associated HPO terms as strings
+     */
     public ArrayList<String> matchVariantWithHpo(String variant) {
-        String geneSymbol = getGene(variant);
+        String geneSymbol = this.getGene(variant);
         return this.getHpo(geneSymbol);
     }
 
-    private static String getGene(String variant) {
+    private String getGene(String variant) {
         String[] splittedLine = variant.split("\t");
         String[] infoString = splittedLine[7].split("\\|");
         return infoString[3];
@@ -31,7 +37,7 @@ public class VariantHpoMatcher {
                     String hpoId = lineSplit[2];
                     String hpoTerm = lineSplit[3];
                     String diseaseId = lineSplit[8];
-                    hpoTerms.add(hpoTerm);
+                    hpoTerms.add(hpoTerm.trim());
                 }
             }
             return hpoTerms;
