6bf5512d6dff161fe8e12ef8e3bf6765bacfd1d6
JonathanKlimp <78917640+JonathanKlimp@users.noreply.github.com>
2022-04-12 14:59:15 +0200

    added .jar of the new version of the HPO annotator. And updated the example pipeline to use the new version

--- builds/pipeline.sh
+++ builds/pipeline.sh
@@ -9,5 +9,5 @@ echo "$filtered_file"
 matched_file=$(java -jar Variant-Matcher-1.0.0.jar "$filtered_file" /Users/jonathan/Documents/GitHub/GenDecS-tools/data/vcfdata.vcf)
 echo "$matched_file"
 
-annotated_file=$(java -jar Variant-HPO-Annotator-1.0.0.jar "$matched_file" /Users/jonathan/Documents/GitHub/GenDecS-tools/data/genes_to_phenotype.txt)
+annotated_file=$(java -jar Variant-HPO-Annotator-1.1.0.jar "$matched_file" /Users/jonathan/Documents/GitHub/GenDecS-tools/data/genes_to_phenotype.txt)
 echo "$annotated_file"
