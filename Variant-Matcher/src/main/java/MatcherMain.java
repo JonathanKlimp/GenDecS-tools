62096d23e497a5fa503675e1265228e7fccf77af
JonathanKlimp <78917640+JonathanKlimp@users.noreply.github.com>
2022-04-14 10:33:01 +0200

    update on the logging and documentation

--- Variant-Matcher/src/main/java/MatcherMain.java
+++ Variant-Matcher/src/main/java/MatcherMain.java
@@ -1,10 +1,19 @@
 import org.apache.commons.cli.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import java.io.File;
+
 
 public class MatcherMain {
-    private static final Logger logger = LoggerFactory.getLogger(MatcherMain.class);
+    private static final Logger logger = LogManager.getLogger(MatcherMain.class);
 
+    /**
+     * Main method for Variant-Matcher.
+     * @param args expected arguments are [location of clinvar] [location of vcf data]
+     *             Optional arguments [--output to give an output directory | --help]
+     * @throws IllegalArgumentException is the given --output is not a directory.
+     */
     public static void main(String[] args) {
         CommandLineParser parser = new DefaultParser();
         Options options = new Options();
@@ -14,26 +23,35 @@ public class MatcherMain {
         try {
             CommandLine commandLine = parser.parse(options, args);
             if (commandLine.hasOption("help")) {
-                HelpFormatter helpFormatter = new HelpFormatter();
-                helpFormatter.printHelp("help", options);
-                System.out.println("program usage: " +
-                        "java -jar Variant-Matcher-[version] [options] [location of filtered clinvar] [location of vcf data]");
-                System.exit(0);
+                printHelp(options);
             }
             String clinvarLocation = commandLine.getArgList().get(0);
             String vcfDataLocation = commandLine.getArgList().get(1);
+            String fileLocation;
             if (commandLine.hasOption("output")) {
+                File outputLoc = new File(commandLine.getOptionValue("output"));
+                if (!outputLoc.isDirectory()) {
+                    throw new IllegalArgumentException("Given output location is not a directory: " + commandLine.getOptionValue("output"));
+                }
                 VariantMatcher variantMatcher = new VariantMatcher(clinvarLocation, vcfDataLocation, commandLine.getOptionValue("output"));
-                String fileLoc = variantMatcher.matchWithClinvar();
-                logger.info("Created the filtered file at the following location: " + fileLoc);
+                fileLocation = variantMatcher.matchWithClinvar();
             } else {
                 VariantMatcher variantMatcher = new VariantMatcher(clinvarLocation, vcfDataLocation, null);
-                String fileLoc = variantMatcher.matchWithClinvar();
-                logger.info("Created the filtered file at the following location: " + fileLoc);
+                fileLocation = variantMatcher.matchWithClinvar();
             }
+            logger.info("Created the filtered file at the following location: " + fileLocation);
+            System.exit(0);
         }
         catch (ParseException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void printHelp(Options options) {
+        HelpFormatter helpFormatter = new HelpFormatter();
+        helpFormatter.printHelp("help", options);
+        System.out.println("program usage: " +
+                "java -jar Variant-Matcher-[version] [options] [location of filtered clinvar] [location of vcf data]");
+        System.exit(0);
     }
 }
--- Variant-Matcher/src/main/java/VariantMatcher.java
+++ Variant-Matcher/src/main/java/VariantMatcher.java
@@ -1,5 +1,5 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -11,12 +11,25 @@ import java.util.regex.Pattern;
 public class VariantMatcher {
     private final String vcfFile;
     private final File clinvarFile;
-    private final String outputLocation;
-    private final String outputLocationClin;
+    private String outputLocation;
+    private String outputLocationClin;
 
-    private static final Logger logger = LoggerFactory.getLogger(VariantMatcher.class);
+    private static final Logger logger = LogManager.getLogger(VariantMatcher.class);
 
+    /**
+     *
+     * @param clinvarLocation Location of the ClinVar file
+     * @param vcfDataLocation Location of the vcf data file
+     * @param outputLocation Location of the desired output directory.
+     *          If null the directory of the vcfDataLocation is used.
+     */
     public VariantMatcher(String clinvarLocation, String vcfDataLocation, String outputLocation) {
+        createOutputLocation(vcfDataLocation, outputLocation);
+        this.vcfFile = vcfDataLocation;
+        this.clinvarFile = new File(clinvarLocation);
+    }
+
+    private void createOutputLocation(String vcfDataLocation, String outputLocation) {
         if (outputLocation == null) {
             String inputFileLoc = vcfDataLocation.replace(".vcf", "");
             String pathName = inputFileLoc + "_clinvar_matched.vcf";
@@ -24,17 +37,13 @@ public class VariantMatcher {
             this.outputLocation = pathName;
             this.outputLocationClin = pathNameClinvar;
         } else {
-            if (!outputLocation.endsWith("/")) {
-                throw new IllegalArgumentException("Given output location is not a directory: " + outputLocation);
-            } else {
                 File vcfFile = new File(vcfDataLocation);
                 String vcfFileName = vcfFile.getName().replace(".vcf", "");
                 this.outputLocation = outputLocation + vcfFileName + "_clinvar_matched.vcf";
                 this.outputLocationClin = outputLocation + vcfFileName + "_clinvar_matched_ClinvarVariants.vcf";
-            }
         }
-        this.vcfFile = vcfDataLocation;
-        this.clinvarFile = new File(clinvarLocation);
+        logger.debug("output location for the clinvar matched variants: " + this.outputLocation);
+        logger.debug("output location for the clinvar variants that are matched: " + this.outputLocationClin);
     }
 
     /**
@@ -52,7 +61,7 @@ public class VariantMatcher {
                 ArrayList<String> header = new ArrayList<>();
                 File vcfFile = new File(this.vcfFile);
                 Scanner reader = new Scanner(vcfFile);
-                logger.info("Building regex of the variants in: " + vcfFile);
+                logger.info("Building regex of the variants in: " + vcfFile.getAbsolutePath());
                 while (reader.hasNextLine()) {
                     String currentLine = reader.nextLine();
                     if (currentLine.startsWith("#")) {
@@ -67,10 +76,10 @@ public class VariantMatcher {
                     Pattern pattern =
                             Pattern.compile(
                                     String.format("%s\\t%s\\t[0-9]+\\t%s\\t%s.+", chromosome, position, ref, alt));
+                    logger.trace("Created the following pattern: " + pattern + "for this line: " + currentLine);
                     stringsToFind.put(currentLine, pattern);
                 }
                 reader.close();
-
                 return getMatchesClinvar(stringsToFind, header);
             } else {
                 return this.outputLocation;
@@ -100,6 +109,7 @@ public class VariantMatcher {
             }
             for (Pattern stringToFind : stringsToFind.values()) {
                 if (currentLine.matches(String.valueOf(stringToFind))) {
+                    logger.trace("Current variant line matched with ClinVar: " + currentLine);
                     writerResult.write(
                             getKeyFromValue(stringsToFind, stringToFind)
                                     + System.getProperty("line.separator"));
