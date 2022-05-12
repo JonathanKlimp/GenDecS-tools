41e941f9211060dde6371a2d4c5601bfcb307ec2
JonathanKlimp <78917640+JonathanKlimp@users.noreply.github.com>
2022-05-12 10:37:48 +0200

    removed pipeline.sh

--- builds/pipeline.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-  set -e
-
-java -jar ClinVar-Filter-2.0.1.jar --output /Users/jonathan/Documents/GitHub/GenDecS-tools/data/ /Users/jonathan/Documents/GitHub/GenDecS-tools/data/clinvar_20220205.vcf
-
-java -jar Variant-Matcher-2.0.1.jar --output /Users/jonathan/Documents/GitHub/GenDecS-tools/data/ /Users/jonathan/Documents/GitHub/GenDecS-tools/data/clinvar_20220205_filtered_ZEROSTAR.vcf /Users/jonathan/Documents/GitHub/GenDecS-tools/data/vcfdata.vcf
-
-java -jar Variant-HPO-Annotator-2.0.1.jar --output /Users/jonathan/Documents/GitHub/GenDecS-tools/data/ /Users/jonathan/Documents/GitHub/GenDecS-tools/data/vcfdata_clinvar_matched.vcf /Users/jonathan/Documents/GitHub/GenDecS-tools/data/genes_to_phenotype.txt
